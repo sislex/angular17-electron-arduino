a327e8777c3901997b1174c1068f4e9a2f96216b
Николай <kolyatola@mail.ru>
2024-02-08 10:49:23 +0300

    remove comment, updated version electron

--- projects/app/src/lib/containers/commands-list-container/commands-list-container.component.ts
+++ projects/app/src/lib/containers/commands-list-container/commands-list-container.component.ts
@@ -4,7 +4,6 @@ import { CommandsListComponent } from '../../../../../ui/src/lib/components/comm
 import { DevicePageLayoutComponent } from '../../../../../ui/src/lib/layouts/device-page-layout/device-page-layout.component';
 import { getCommandsList } from '../../+state/commands-list/commands-list.selectors';
 import { Store } from '@ngrx/store';
-import { CommandsListState } from '../../+state/commands-list/commands-list.reducer';
 import { ActivatedRoute } from '@angular/router';
 import { sendMessage } from '../../+state/messages/messages.actions';
 
@@ -27,28 +26,6 @@ export class CommandsListContainerComponent {
     private readonly store: Store,
     ) {}
 
-
-  // events($event: any) {
-  //   // console.log($event);
-  //   if ($event.event === 'UsbListContainerComponent:BUTTON_CLICKED' && $event.data.message === 'CONNECT_USB_DEVICE') {
-  //     const now = new Date();
-  //     const timestamp = now.getTime();
-  //     const name =  $event.data.note.item.name;
-  //     this.store.dispatch(sendMessage({message: {event: 'CONNECT_USB_DEVICE', data: {name, timestamp}}}));
-  //   } else if ($event.event === 'UsbListContainerComponent:BUTTON_CLICKED' && $event.data.message === 'DISCONNECT_USB_DEVICE') {
-  //     const now = new Date();
-  //     const timestamp = now.getTime();
-  //     const name =  $event.data.note.item.name;
-  //     this.store.dispatch(sendMessage({message: {event: 'DISCONNECT_USB_DEVICE', data: {name, timestamp}}}));
-  //   } else if ($event.event === 'UsbListContainerComponent:BUTTON_CLICKED' && $event.data.message === 'CONTROL') {
-  //     this.router.navigate(['control', 'blink', $event.data.note.item.name]);
-  //     setTimeout(() => { // lazy load
-  //       this.store.dispatch(setSelectedUsb({selectedUsb: $event.data.note.item.name}));
-  //     }, 100);
-  //   }
-
-
-  
   buttonClick($event: any) {
     // console.log($event);
     this.store.dispatch(sendMessage({
