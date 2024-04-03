0e9189ac4560eb3c83fc150a17e074081c62c2bd
Nikolay <work.mail.tola@gmail.com>
2024-04-03 12:15:39 +0300

    finished targets interface

--- projects/tripod/src/lib/+state/skins/move-skin/targets/targets.reducer.ts
+++ projects/tripod/src/lib/+state/skins/move-skin/targets/targets.reducer.ts
@@ -1,5 +1,4 @@
 import { createReducer, on } from '@ngrx/store';
-// import * as BlinkMessagesActions from './targets.actions';
 
 export const TRIPOD_TARGETS_FEATURE_KEY = 'tripod/messages';
 
@@ -20,16 +19,12 @@ export interface IDataTarget {
   height: number;
 }
 
-
 export interface TripodTargetsPartialState {
   readonly [TRIPOD_TARGETS_FEATURE_KEY]: ITargetsState;
 }
 
 export const initialState: ITargetsState = {
-  target: [
-    {targetName: 'target1', posData: [ {xPos: 5, yPos: 5,  width:5, height: 5} ], speedVerification: 0},
-    {targetName: 'target2', posData: [ {xPos: 10, yPos: 10,  width:10, height: 10} ], speedVerification: 1},
-  ]
+  target: []
 };
 
 export const TargetsReducer = createReducer(
--- projects/tripod/src/lib/+state/skins/move-skin/targets/targets.selectors.ts
+++ projects/tripod/src/lib/+state/skins/move-skin/targets/targets.selectors.ts
@@ -1,4 +1,4 @@
-import {createFeatureSelector, createSelector} from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { TRIPOD_TARGETS_FEATURE_KEY, ITargetsState } from './targets.reducer';
 
 export const selectFeature = createFeatureSelector<ITargetsState>(TRIPOD_TARGETS_FEATURE_KEY);
